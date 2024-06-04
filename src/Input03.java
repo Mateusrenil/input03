86bdc05849d7083e9bea1399d99d29daa4ea17ac
Aluno <Aluno@10.85.198.41>
2024-06-04 16:50:11 -0300

    Commit project Input03

---
+++ src/Input03.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+class Input03 {
+
+    public static void main(String[] args) {
+        //Create a Scanner
+    Scanner scanner = new Scanner (System.in);
+    int soma = 0;
+        
+    soma += scanner.nextInt();
+    soma += scanner.nextInt();
+    soma += scanner.nextInt();
+        
+        
+    scanner.close();
+    
+    System.out.println(" a soma dos 3 primeiro numero é : " + soma);
+    //Find and print the sum of three integers entered by the user
+        
+        
+        
+        
+        
+        //Remember to close the Scanner
+        
+    }
+}
